072a5e3d4f186c45821c27fd671763b29080684c
davelimutin <dave.limutin.pixel8@gmail.com>
2022-10-08 06:12:37 +0800

    gallery page finished, candle-light page finished

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -2,10 +2,10 @@ function changeBg(){
     var navbar = document.getElementById('nav-bar');
     var scrollValue = window.scrollY;
     console.log(scrollValue);
-    if(scrollValue > 700 && scrollValue < 1405){
+    if(scrollValue >= 0 && scrollValue < 645){
         navbar.classList.add('no-opacity');
         navbar.classList.remove('no-opacity-2');
-    }else if(scrollValue > 1405 && !(scrollValue <= 1405)){
+    }else if(scrollValue > 645 && !(scrollValue <= 645)){
         navbar.classList.add('no-opacity-2');
         navbar.classList.remove('no-opacity');
     }
@@ -19,11 +19,11 @@ function changePosition(){
     var invisible = document.getElementById('gallery-invisible');
     var bottom = document.getElementById('gallery-bottom');
     var scrollValue = window.scrollY;
-    if(scrollValue > 1405 && !(scrollValue <= 1405) && !(scrollValue > 2024 )){
+    if(scrollValue > 645 && !(scrollValue <= 645) && !(scrollValue > 1274 )){
         position.classList.add('fixed-effect');
         invisible.classList.add('gallery-is-visible');
         position.classList.remove('gallery-is-bottom');
-    }else if(scrollValue > 2024 && !(scrollValue <= 2024)){
+    }else if(scrollValue > 1274 && !(scrollValue <= 1274)){
         position.classList.add('gallery-is-bottom');
         position.classList.remove('fixed-effect');
         invisible.classList.remove('gallery-is-visible');
